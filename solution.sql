08cfeff70a13e7aa27796d4e259e3efb5ab1c750
Daniela Matos <daniela.lmatos@gmail.com>
2020-01-28 10:35:07 -0600

    Queries complited

--- solution.sql
+++ solution.sql
@@ -8,7 +8,7 @@ FROM sakila.actor;
 
 SELECT first_name,
 		last_name,
-        CONCAT (first_name , ' ',last_name) AS Actor_Name
+        UPPER (CONCAT (first_name , ' ',last_name)) AS Actor_Name
 FROM sakila.actor;	
 
 -- You need to find the ID number, first name, and last name of an actor, 
@@ -73,7 +73,8 @@ HAVING COUNT(last_name) >= 2;
 
 UPDATE sakila.actor
 SET first_name = 'HARPO'
-WHERE first_name = 'GROUCHO';
+WHERE first_name = 'GROUCHO'
+AND last_name = 'Williams';
 
 -- Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct 
 -- name after all! In a single query, if the first name of the actor is currently HARPO, change it 
@@ -81,13 +82,33 @@ WHERE first_name = 'GROUCHO';
 
 UPDATE sakila.actor
 SET first_name = 'GROUCHO'
-WHERE first_name = 'HARPO';
+WHERE first_name = 'HARPO'
+AND last_name = 'williams';
 
 -- You cannot locate the schema of the address table. Which query would you use to re-create it?
 
+SHOW CREATE TABLE sakila.address;
 
-
-
+-- CREATE TABLE IF NOT EXISTS `sakila`.`address` (
+ -- `address_id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  -- `address` VARCHAR(50) NOT NULL,
+  -- `address2` VARCHAR(50) NULL DEFAULT NULL,
+  -- `district` VARCHAR(20) NOT NULL,
+  -- `city_id` SMALLINT(5) UNSIGNED NOT NULL,
+ --  `postal_code` VARCHAR(10) NULL DEFAULT NULL,
+  -- `phone` VARCHAR(20) NOT NULL,
+  -- `location` GEOMETRY NOT NULL,
+  -- `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  -- PRIMARY KEY (`address_id`),
+  -- INDEX `idx_fk_city_id` (`city_id` ASC),
+  -- SPATIAL INDEX `idx_location` (`location` ASC),
+  -- CONSTRAINT `fk_address_city`
+   --  FOREIGN KEY (`city_id`)
+    -- REFERENCES `sakila`.`city` (`city_id`)
+    -- ON UPDATE CASCADE)
+-- ENGINE = InnoDB
+-- AUTO_INCREMENT = 606
+-- DEFAULT CHARACTER SET = utf8
 
 -- Use JOIN to display the first and last names, as well as the address, of each staff member. 
 -- Use the tables staff and address:
@@ -100,10 +121,11 @@ USING (address_id);
 -- Use JOIN to display the total amount rung up by each staff member in August of 2005. 
 -- Use tables staff and payment.
 
-SELECT staff_id, first_name, last_name, SUM(amount)
-FROM sakila.staff 
-JOIN sakila.payment 
+SELECT staff_id, first_name, last_name, SUM(amount) AS total_amount
+FROM sakila.staff s
+JOIN sakila.payment p
 USING (staff_id)
+WHERE p.payment_date LIKE '2005-08%'
 GROUP BY staff_id;
 
 -- List each film and the number of actors who are listed for that film. 
@@ -159,3 +181,101 @@ WHERE title = 'Alone Trip';
 -- for which you will need the names and email addresses of all Canadian customers. 
 -- Use joins to retrieve this information.
 
+SELECT first_name, last_name, email, country
+FROM sakila.customer
+JOIN sakila.address
+USING (address_id)
+JOIN sakila.city
+USING (city_id)
+JOIN sakila.country
+USING (country_id)
+WHERE country = 'Canada';
+
+-- Sales have been lagging among young families, and you wish to target all family movies 
+-- for a promotion. Identify all movies categorized as family films.
+
+SELECT title, name AS category 
+FROM sakila.film
+JOIN sakila.film_category
+USING (film_id)
+JOIN sakila.category
+USING (category_id)
+WHERE category_id = 8;
+
+-- Display the most frequently rented movies in descending order.
+
+SELECT title, COUNT(rental_date) AS total_rental_days
+FROM film
+JOIN inventory
+USING (film_id)
+JOIN rental
+USING (inventory_id)
+GROUP BY title
+ORDER BY total_rental_days DESC;
+
+-- Write a query to display how much business, in dollars, each store brought in.
+
+SELECT store_id, CONCAT('$ ', FORMAT(SUM(amount), 2)) AS total_amount
+FROM sakila.payment
+JOIN sakila.staff
+USING (staff_id)
+JOIN sakila.store
+USING (store_id)
+GROUP BY store_id;
+
+-- Write a query to display for each store its store ID, city, and country.
+
+SELECT store_id, city, country
+FROM sakila.store
+JOIN address
+USING (address_id)
+JOIN city
+USING (city_id)
+JOIN country
+USING (country_id);
+
+-- List the top five genres in gross revenue in descending order. 
+-- (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
+SELECT name AS genres, CONCAT('$ ', FORMAT(SUM(amount), 2)) AS gross_revenue
+FROM category
+JOIN film_category
+USING (category_id)
+JOIN inventory
+USING (film_id)
+JOIN rental
+USING (inventory_id)
+JOIN payment
+USING (rental_id)
+GROUP BY name
+ORDER BY gross_revenue DESC
+LIMIT 5;
+
+-- In your new role as an executive, you would like to have an easy way of viewing the 
+-- Top five genres by gross revenue. Use the solution from the problem above to 
+-- create a view. If you haven't solved 7h, you can substitute another query to create a view.
+
+CREATE VIEW sakila.top_five_genre (Genres, Gross_Revenue)
+AS
+SELECT name AS Genres, CONCAT('$ ', FORMAT(SUM(amount), 2)) AS Gross_Revenue
+FROM payment
+JOIN rental
+USING (rental_id)
+JOIN inventory
+USING (inventory_id)
+JOIN film_category
+USING (film_id)
+JOIN category
+USING (category_id)
+GROUP BY name
+ORDER BY Gross_Revenue DESC
+LIMIT 5;
+
+-- How would you display the view that you created in 8a?
+
+SELECT *
+FROM sakila.top_five_genre;
+
+-- You find that you no longer need the view top_five_genres. Write a query to delete it.
+
+DROP VIEW sakila.top_five_genre;
